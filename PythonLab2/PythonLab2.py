1d27beebb22c879972a0b82dccbd15808ea4d7f9
Max1Voz <92223897+MaximVozisov@users.noreply.github.com>
2023-01-20 13:44:11 +0500

    Изменение кодировки

--- PythonLab2/PythonLab2.py
+++ PythonLab2/PythonLab2.py
@@ -1,9 +1,9 @@
-# -*- coding: utf_8 -*-
+# -*- coding: cp1251 -*-
 
 from pyDatalog import pyDatalog 
 import random
 
-#Посчитать сумму ряда 
+#РџРѕСЃС‡РёС‚Р°С‚СЊ СЃСѓРјРјСѓ СЂСЏРґР° 
 pyDatalog.create_terms('arrSum, N, AVS, arrRandom, arrRandom2, arrRandomMedian')
 
 arrSum[N] = N + arrSum[N-1]
@@ -11,16 +11,16 @@ arrSum[1] = 1
 
 print(arrSum[100]==N)
 
-# Среднее значение ряда
+# РЎСЂРµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ СЂСЏРґР°
 AVS[N] = (N + 1)/2
 print(AVS[200]==N)
 
-# Сумма ряда случайных чисел [1,100]
+# РЎСѓРјРјР° СЂСЏРґР° СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР» [1,100]
 arrRandom[N] = random.randint(0,100) + arrRandom[N - 1]
 arrRandom[1] = random.randint(0, 100)
 print(arrRandom[100]==N)
 
-#Медиана ряда случайных чисел
+#РњРµРґРёР°РЅР° СЂСЏРґР° СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР»
 arrRandom2[N] = random.randint(0,100) + arrRandom2[N - 1]
 arrRandom2[1] = random.randint(0, 100)
 arrRandomMedian[N] = arrRandom2[N] / N
