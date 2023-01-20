4287d11256e6f0706cdcadd763201dc59a20547f
Max1Voz <92223897+MaximVozisov@users.noreply.github.com>
2023-01-20 13:49:32 +0500

    Update mammal.py

--- PythonLab1/mammal.py
+++ PythonLab1/mammal.py
@@ -6,7 +6,7 @@ class Mammal(Animal):
     def __init__(self, name = "",age = 0,gender = "", view = ""):
         super().__init__(name, age, gender)
         self.view = view
-    def __repr__(self): # отображение объекта в режиме отладки
+    def __repr__(self): # РѕС‚РѕР±СЂР°Р¶РµРЅРёРµ РѕР±СЉРµРєС‚Р° РІ СЂРµР¶РёРјРµ РѕС‚Р»Р°РґРєРё
         return self.__str__()
     def __str__(self):
         return str(self.__dict__)
